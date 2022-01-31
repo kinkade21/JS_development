775653a0eb97d18430a3f00b25336a2ee03f605c
kinkade21 <victorpushanov@mail.ru>
2022-01-31 21:12:46 +0300

    выполнил второе дз

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let title;
-let screens;
-let  screenPrice;
-let  rollback;
-let  fullPrice;
-let  adaptive;
+const title = "newProject";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 1271;
+const rollback = 67;
+const fullPrice = 371458;
+const adaptive = false;
 
-alert("hello world!");
-
-console.log("hello world!!!");
+console.log(typeof title, screens, screenPrice);
+console.log(screens.length);
+console.log("Стоимость вертски экранов", screenPrice, "рублей");
+console.log("Стоимость разработки сайта", fullPrice, "рублей");
+console.log(screens.toLowerCase().split(","));
+console.log(fullPrice * (rollback/100), "рублей");
